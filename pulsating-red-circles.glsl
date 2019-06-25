1bfde1c5aaa693528099d1f5fca23cf6ce057894
Nikita Karetnikov <nikita@karetnikov.org>
2019-06-25 17:35:12 +0300

    Add pulsating red circles

---
+++ pulsating-red-circles.glsl
@@ -0,0 +1,11 @@
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Coordinates of the current pixel.
+    vec2 xy = fragCoord.xy;
+
+    // Divide coordinates by the screen resolution.
+    xy.x = xy.x / iResolution.x;
+    xy.y = xy.y / iResolution.y;
+
+    fragColor = vec4(sin(mod(100000.0 + iGlobalTime, 1000000.0) * (pow(1.0 + xy.x, 2.0) + pow(1.0 + xy.y, 2.0))), 0, 0, 1.0);
+}
